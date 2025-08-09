f9c1dd1ea81f6e27299fc5fd61be3ea36f957877
Your Name <you@example.com>
2025-08-09 12:58:25 +0530

    my changes

--- springbootproject/src/main/java/com/training/GreetDevopsApplication.java
+++ springbootproject/src/main/java/com/training/GreetDevopsApplication.java
@@ -17,6 +17,7 @@ public class GreetDevopsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GreetDevopsApplication.class, args);
 		System.out.println("hello uday");
+		System.out.println("hello uday kiran");
 	}
 
 }
